2901ae84015b88eda5518f3cca0999cb87b3bb4d
hgorges <gorges@cryptospace.dev>
2024-09-20 01:05:19 +0200

    Using std:: for conformity

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 
 int main(const int argc, char **argv) {
   if (argc != 2) {
-    printf("Usage: %s <path_to_image>\n", argv[0]);
+    std::printf("Usage: %s <path_to_image>\n", argv[0]);
     return -1;
   }
 
   const char *path = argv[1];
   const cv::Mat image = cv::imread(path, cv::IMREAD_GRAYSCALE);
   if (!image.data) {
-    printf("No image data found at '%s'\n", path);
+    std::printf("No image data found at '%s'\n", path);
     return -1;
   }
 
